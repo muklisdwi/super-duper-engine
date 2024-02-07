aa3a6e391494446bff8c577aac5ce981053d73a4
muklisdwi <muklisdwi527@gmail.com>
2024-02-07 14:05:21 +0700

    create migration and update makefile

---
+++ migrations/000001_initialize_schema.up.sql
@@ -0,0 +1,5 @@
+-- use database / schema
+-- reference https://github.com/golang-migrate/migrate
+-- $ migrate -source  -database user:password@tcp(host:port)/dbname?multiStatements=true up 2
+
+USE library;
